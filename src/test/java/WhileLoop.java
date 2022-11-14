0e20f69dbc0d09ac21f635d7424437e07f6b6a70
jvnaidu <jvnaidu07@gmail.com>
2022-11-14 22:02:14 +0530

    save

--- src/test/java/WhileLoop.java
+++ src/test/java/WhileLoop.java
@@ -5,6 +5,8 @@ public class WhileLoop {
             System.out.println("the value of:"+a);
             a++;
             System.out.println("\n");
+            System.out.println((a));
         }
     }
 }
+
